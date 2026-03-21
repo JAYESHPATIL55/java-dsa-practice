de5dadf190163241726cacee941a0213e260d1ad
JAYESHPATIL55 <jayeshpatil8744@gmail.com>
2026-03-21 23:32:26 +0530

    practicing the math based program

---
+++ maths/GCD.java
@@ -0,0 +1,20 @@
+package maths;
+
+public class GCD {
+
+    static int hcf(int a, int b) {
+        int result = Math.min(a, b);
+        while (result > 0) {
+            if (a % result == 0 && b % result == 0) {
+                break;
+            }
+            result--;
+        }
+        return result;
+    }
+
+    public static void main(String... args) {
+        int a = 20, b = 10;
+        System.out.println(hcf(a, b));
+    }
+}
---
+++ maths/Reverse.java
@@ -0,0 +1,18 @@
+package maths;
+
+public class Reverse {
+
+    static int rev(int num) {
+        int rev = 0;
+        while (num > 0) {
+            rev = rev * 10 + num % 10;
+            num = num / 10;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        int num = 34;
+        System.out.println(rev(num));
+    }
+}
